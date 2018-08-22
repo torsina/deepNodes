444defc9d653bc22b82e334eb6e1e1d25edbcdcd
torsina <theo.sagot.1@gmail.com>
2018-08-23 01:46:03 +0200

    Layer#buildLayer done

--- src/Layer.js
+++ src/Layer.js
@@ -3,6 +3,7 @@ class Layer {
 		this._deepNodes = deepNodes;
 		this._map = new Map();
 		this._compiledPermArray = [];
+		this._compiledPermMapArray = new Map();
 	}
 	addItem(id, item) {
 		this._map.set(id, item);
@@ -15,43 +16,39 @@ class Layer {
 		return true;
 	}
 	buildLayer() {
-		if(this._deepNodes._useIndex) {
-			this._map.forEach((permArray) => {
-				for(let i = 0, n = permArray.length; i < n; i++) {
-					const perm = permArray[i];
-					const deny = perm.charAt(0) === "-";
-					const positivePerm = deny ? perm.slice(1) : perm;
-					const negativePerm = deny ? perm : null;
-					const positiveIndex = this._compiledPermArray.indexOf(positivePerm);
-					const negativeIndex = this._compiledPermArray.indexOf(negativePerm);
-					/*
-					deny + positive
-					deny + negative
-					deny + nothing
-					!deny + positive
-					!deny + negative
-					!deny + nothing
-					 */
-					switch(deny) {
-						case true: {
-							// if positive index !== 0
-							if(positiveIndex) {
-								this._compiledPermArray
-									.splice(positiveIndex, 1)
-									.push(perm);
-							} else if(negativeIndex) {
-								// nothing
-							}
-							break;
-						}
-						case false: {
-							
-							break;
-						}
-					}
+		this._map.forEach((permArray) => {
+			for(let i = 0, n = permArray.length; i < n; i++) {
+				const perm = permArray[i];
+				const deny = perm.charAt(0) === "-";
+				let positivePerm, positiveIndex;
+				let negativePerm, negativeIndex;
+				// only search for the needed indexes
+				if(deny) {
+					positivePerm = deny ? perm.slice(1) : perm;
+					positiveIndex = this._compiledPermMapArray.get(positivePerm);
+				} else {
+					negativePerm = deny ? perm : null;
+					negativeIndex = this._compiledPermMapArray.get(negativePerm);
 				}
-			});
-		}
+				// no index on both sides
+				if(!positiveIndex && !negativeIndex) {
+					// add the perm to the array and the map
+					this._compiledPermMapArray.set(perm, this._compiledPermArray.length);
+					this._compiledPermArray.push(perm);
+				} else if(!deny && !!negativeIndex) {
+					// there is negative index + allow
+					this._compiledPermArray[negativeIndex] = perm;
+					this._compiledPermMapArray.delete(negativePerm);
+					this._compiledPermMapArray.set(perm, negativeIndex);
+				} else if(deny && !!positiveIndex) {
+					// we edit the map and the array
+					// from a positive perm to a negative perm
+					this._compiledPermArray[positiveIndex] = perm;
+					this._compiledPermMapArray.delete(positivePerm);
+					this._compiledPermMapArray.set(perm, positiveIndex);
+				}
+			}
+		});
 	}
 }
 module.exports = Layer;
