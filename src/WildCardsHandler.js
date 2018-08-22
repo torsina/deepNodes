81b37ac072d480948e96bce343d271d0cb6b3814
torsina <theo.sagot.1@gmail.com>
2018-08-23 01:46:57 +0200

    rewrite WildCardsHandler#starHandler

--- src/WildCardsHandler.js
+++ src/WildCardsHandler.js
@@ -5,39 +5,9 @@ class wildCardsHandler {
 		this._decomposedPermissionArray = deepNodes._decomposedPermissionArray;
 	}
 	mainHandler(permission) {
-		return this.foo(permission);
+		return this.starHandler(permission);
 	}
 	starHandler(permission) {
-		const decomposedPermission = permission.split(".");
-		const starIndexes = [];
-		const resultCache = this._decomposedPermissionArray.slice(0);
-		const results = [];
-		for(let i = 0, n = decomposedPermission.length; i < n; i++) {
-			const node = decomposedPermission[i];
-			if(node === "*") starIndexes.push(i);
-		}
-		// loop for each star found
-		for(let i = 0, n = starIndexes.length; i < n; i++) {
-			const starIndex = starIndexes[i];
-			console.log("starIndex " + starIndex);
-			// loop for each tier of node up to the one of the star
-			for(let j = 0; j < starIndex; j++) {
-				console.log("searchTierIndex " + j);
-				// loop to search on the cache
-				for(let k = 0; k < resultCache.length; k++) {
-					console.log(resultCache);
-					const searchedPerm = resultCache[k];
-					console.log("searchedPerm " + searchedPerm);
-					if(decomposedPermission[j] !== searchedPerm[j]) {
-						resultCache.splice(k, 1);
-						k--;
-					}
-				}
-			}
-		}
-		return resultCache;
-	}
-	foo(permission) {
 		const decomposedPermission = permission.split(".");
 		const results = this._decomposedPermissionArray.slice(0);
 		for(let i = 0, n = decomposedPermission.length; i < n; i++) {
